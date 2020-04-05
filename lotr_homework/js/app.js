d4f58b8827a962885b003251a83726b9c33dd703
yhussin <yhussin@gmail.com>
2020-04-05 16:36:11 -0700

    Chapter 1 complete - Made Middle Earth

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -47,19 +47,20 @@ const makeMiddleEarth = () => {
   console.log("Trying to make middle earth.");
 
   // 1. create a section tag with an id of middle-earth
-
+  const sectionMiddleEarth = $('<section id="#middle-earth"/>')
   // 2. append the section to the body of the DOM.
-
+  $('body').append(sectionMiddleEarth);
   // 3. use a for loop to iterate over the lands array that does the following:
-
+  for (let i = 0; i<lands.length; i++) {
   //   3a. creates an article tag (there should be one for each land when the loop is done)
-
+  const articleMiddleEarth = $('<article/>')
   //   3b. gives each land article an `id` tag of the corresponding land name
-
+   articleMiddleEarth.attr('id', lands[i]);
   //   3c. includes an h1 with the name of the land inside each land article
-
+   articleMiddleEarth.html('<h1>' + lands[i] + '</h1>');
   //   3d. appends each land to the middle-earth section
-
+   sectionMiddleEarth.append(articleMiddleEarth);
+  }
 };
 
 // COMMIT YOUR WORK
@@ -73,7 +74,7 @@ const makeHobbits = () => {
   console.log('Make hobbits');
 
   // 1. display an unordered list of the hobbits in the shire.
-
+  $('ul').add('hobbits')
   // 2. give each hobbit a class of "hobbit"
 
   // hint: create a 'ul' outside the loop upon which to append the 'li's
